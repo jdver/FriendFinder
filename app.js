dd2b64a73a118a6d325bcaebd1a38183cb000b1e
JD Vera <jdvera1@gmail.com>
2019-06-24 18:57:30 -0700

    modal working and results displaying

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var express = require("express");
-var app = express();
-var path = require('path');
-
+var express = require('express')
+var app = express()
+/* eslint-disable no-unused-vars */
+var path = require('path')
+/* eslint-enable no-unused-vars */
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8080
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require('./app/routing/apiRoutes')(app)
+require('./app/routing/htmlRoutes')(app)
 
-
-app.listen(PORT, function() {
-  console.log(`App listening on PORT ${PORT}`);
-});
+app.listen(PORT, function () {
+  console.log(`App listening on PORT ${PORT}`)
+})
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,85 +1,85 @@
 var friends = [
-	
-	{
-		name: "Charles Xavier",
-		photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		scores: [
-			"5",
-			"1",
-			"4",
-			"3",
-			"1",
-			"5",
-			"1",
-			"4",
-			"3",
-			"1"
-		]
-	},
-	{
-		name: "Aquaria",
-		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		scores: [
-			"3",
-			"1",
-			"5",
-			"4",
-			"1",
-			"5",
-			"1",
-			"1",
-			"5",
-			"3"
-		]
-	},
-	{
-		name: "Unknown",
-		photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		scores: [
-			"3",
-			"1",
-			"4",
-			"1",
-			"4",
-			"3",
-			"5",
-			"1",
-			"5",
-			"4"		
-		]
-  },
-  {
-		name: "Donald Duck",
-		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		scores: [
-			"2",
-			"3",
-			"2",
-			"5",
-			"4",
-			"5",
-			"1",
-			"2",
-			"3",
-			"5"
-		]
-  },
-  {
-		name: "Mimi Imfurst",
-		photo: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-		scores: [
-			"4",
-			"3",
-			"4",
-			"4",
-			"1",
-			"5",
-			"5",
-			"2",
-			"2",
-			"1"
-		]
-	},
-];
 
-module.exports = friends;
+  {
+    name: 'Charles Xavier',
+    photo: 'https://www.pexels.com/photo/man-wearing-plaid-dress-shirt-769733/?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    scores: [
+      '5',
+      '1',
+      '4',
+      '3',
+      '1',
+      '5',
+      '1',
+      '4',
+      '3',
+      '1'
+    ]
+  },
+  {
+    name: 'Aquaria',
+    photo: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    scores: [
+      '3',
+      '1',
+      '5',
+      '4',
+      '1',
+      '5',
+      '1',
+      '1',
+      '5',
+      '3'
+    ]
+  },
+  {
+    name: 'Unknown',
+    photo: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    scores: [
+      '3',
+      '1',
+      '4',
+      '1',
+      '4',
+      '3',
+      '5',
+      '1',
+      '5',
+      '4'
+    ]
+  },
+  {
+    name: 'Donald Duck',
+    photo: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    scores: [
+      '2',
+      '3',
+      '2',
+      '5',
+      '4',
+      '5',
+      '1',
+      '2',
+      '3',
+      '5'
+    ]
+  },
+  {
+    name: 'Mimi Imfurst',
+    photo: 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    scores: [
+      '4',
+      '3',
+      '4',
+      '4',
+      '1',
+      '5',
+      '5',
+      '2',
+      '2',
+      '1'
+    ]
+  }
+]
+
+module.exports = friends
